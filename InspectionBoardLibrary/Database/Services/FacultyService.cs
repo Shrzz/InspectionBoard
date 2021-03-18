98adf982bcc601aadbf300961d5832ff8198972e
Shrzz <shrzz@inbox.ru>
2021-03-18 23:33:16 +0300

    implemented faculty and user services, removed dbc

---
+++ InspectionBoardLibrary/Database/Services/FacultyService.cs
@@ -0,0 +1,30 @@
+﻿using InspectionBoardLibrary.Models.DatabaseModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InspectionBoardLibrary.Database.Services
+{
+    public static class FacultyService
+    {
+        public async static Task AddSync(Faculty o)
+        {
+            using (ExamContext context = new ExamContext())
+            {
+                context.Faculties.Add(o);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public static List<Faculty> Select()
+        {
+            using (ExamContext context = new ExamContext())
+            {
+                return context.Faculties.AsNoTracking().AsNoTracking().ToListAsync().Result;
+            }
+        }
+    }
+}
---
+++ InspectionBoardLibrary/Database/Services/UserService.cs
@@ -0,0 +1,21 @@
+﻿using InspectionBoardLibrary.Models.DatabaseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InspectionBoardLibrary.Database.Services
+{
+    public static class UserService
+    {
+        public async static Task AddUser(User u)
+        {
+            using (UserContext context = new UserContext())
+            {
+                context.Users.Add(u);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
